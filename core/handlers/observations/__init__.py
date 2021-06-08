8e574d864dca8795650429d2de3d06ef959443d9
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2021-06-08 17:29:13 +0900

    WIP

--- core/handlers/observations/__init__.py
+++ core/handlers/observations/__init__.py
@@ -10,14 +10,13 @@ from core.worlds.abstract_world import AbstractWorld
 from omegaconf import DictConfig
 
 from .abstract_observation import AbstractObservation
+from .local_transition_observation import LocalTransitionObservation
 from .local_view_observation import LocalViewObservaton
 from .relative_view_observation import RelativeViewObservaton
-from .local_transition_observation import LocalTransitionObservation
-
 
 logger = initialize_logging(__name__)
 
-__all__ = ["AbstractObservation", "LocalViewObservation", "RelativeViewObservation"]
+__all__ = ["AbstractObservation", "LocalViewObservation", "RelativeViewObservation", "LocalTransitionObservation"]
 
 
 def generate_observation_handler(
