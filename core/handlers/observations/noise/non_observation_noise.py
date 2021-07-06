b078df3bcfc2c196d19ead55bc611f38f7442c6e
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2021-07-06 13:30:22 +0900

    non-observation-noise added

--- core/handlers/observations/noise/non_observation_noise.py
+++ core/handlers/observations/noise/non_observation_noise.py
@@ -6,9 +6,13 @@ That is, no noise is added in the observation phase.
 Author: Yoshinari Motokawa <yoshinari.moto@fuji.waseda.jp>
 """
 
+import torch
+
 from .abstract_observation_noise import AbstractObservationNoise
 
 
 class NonObservationNoise(AbstractObservationNoise):
-    def add_noise(self, obs, agent, agent_id, offset_x, offset_y):
-        return obs
+    def get_noise(self, agent, agent_id, offset_x, offset_y):
+        noise = torch.zeros(self.observation_space)
+
+        return noise
