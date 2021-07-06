6a3379107411617efda4c3e17283966871d4216e
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2021-07-06 13:55:35 +0900

    ThresholdDistObservationNoise added

--- core/handlers/observations/noise/__init__.py
+++ core/handlers/observations/noise/__init__.py
@@ -12,7 +12,8 @@ from core.worlds.abstract_world import AbstractWorld
 from omegaconf import DictConfig
 
 from .abstract_observation_noise import AbstractObservationNoise
-from .dist_observation_noise import DistObservatonNoise
+from .dist_observation_noise import (DistObservatonNoise,
+                                     ThresholdDistObservationNoise)
 from .flat_observation_noise import FlatObservatonNoise
 from .non_observation_noise import NonObservationNoise
 
@@ -22,7 +23,8 @@ __all__ = [
     "AbstractObservationNoise",
     "NonObservationNoise",
     "DistObservatonNoise",
-    "FlatObservatonNoise"
+    "FlatObservatonNoise",
+    "ThresholdDistObservationNoise"
 ]
 
 
@@ -32,7 +34,10 @@ def generate_observation_noise(config: DictConfig, world: AbstractWorld, observa
 
     elif config.observation_noise == "sensing_dist":
         noise = DistObservatonNoise(config=config, world=world, observation_space=observation_space)
-    
+
+    elif config.observation_noise == "threshold_sensing_dist":
+        noise = ThresholdDistObservationNoise(config=config, world=world, observation_space=observation_space)
+
     elif config.observation_noise == 'flat':
         noise = FlatObservatonNoise(config=config, world=world, observation_space=observation_space)
 
