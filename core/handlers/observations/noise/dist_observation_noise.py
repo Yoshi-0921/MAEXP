95795c941d9187b026efec1323cf0769557929f6
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2021-07-06 13:58:51 +0900

    Dist noise updated

--- core/handlers/observations/noise/dist_observation_noise.py
+++ core/handlers/observations/noise/dist_observation_noise.py
@@ -19,13 +19,37 @@ from .abstract_observation_noise import AbstractObservationNoise
 class DistObservatonNoise(AbstractObservationNoise):
     def __init__(self, config: DictConfig, world: AbstractWorld, observation_space: List[int]):
         super().__init__(config=config, world=world, observation_space=observation_space)
-        self.probability_distribution = None
+        self.std_distribution = config.std_distribution
 
     def get_noise(self, agent, agent_id, offset_x, offset_y):
         noise = torch.zeros(self.observation_space)
         _, x, y = self.observation_space
         for i in range(min(x, y) // 2):
-            noise[:, i:x, i:y] = torch.empty((3, (x - i), (y - i))).normal_(mean=0, std=0.01 * min(x, y))
+            noise[:, i:x, i:y] = torch.empty((self.observation_space[0], (x - i), (y - i))).normal_(mean=0, std=self.std_distribution[i])
+            x -= 1
+            y -= 1
+
+        return noise
+
+
+class ThresholdDistObservationNoise(DistObservatonNoise):
+    def __init__(self, config: DictConfig, world: AbstractWorld, observation_space: List[int]):
+        super().__init__(config=config, world=world, observation_space=observation_space)
+        self.threshold_ratio = config.noise_threshold_ratio
+
+    def get_noise(self, agent, agent_id, offset_x, offset_y):
+        noise = torch.zeros(self.observation_space)
+        _, x, y = self.observation_space
+        for i in range(min(x, y) // 2):
+            generated_noise = torch.empty((self.observation_space[0], (x - i), (y - i))).normal_(mean=0, std=self.std_distribution[i])
+            noise[:, i:x, i:y] = torch.where(
+                generated_noise < -self.std_distribution[i] * self.threshold_ratio,
+                generated_noise, noise[:, i:x, i:y]
+            )
+            noise[:, i:x, i:y] = torch.where(
+                self.std_distribution[i] * self.threshold_ratio < generated_noise,
+                generated_noise, noise[:, i:x, i:y]
+            )
             x -= 1
             y -= 1
 
