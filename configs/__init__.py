7a5517c9323e02b7e2dfd838cd8214d76f891e04
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2022-02-28 10:22:50 +0900

    config added

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,1 +1,1 @@
-config_names = {"example": "experiments/example", "da3": "experiments/da3_exp", "da3_types": "experiments/da3_types_exp"}
+config_names = {"example": "experiments/example", "da3": "experiments/da3_exp", "da3_types": "experiments/da3_types_exp", "da6": "experiments/da6_exp"}
