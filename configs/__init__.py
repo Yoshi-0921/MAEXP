1463090735e1c7cd788423838c498ae6d1c2c6db
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2022-06-03 14:03:02 +0900

    added dra3 config

--- configs/__init__.py
+++ configs/__init__.py
@@ -2,5 +2,6 @@ config_names = {
     "example": "experiments/example",
     "da3": "experiments/da3_exp",
     "da3_types": "experiments/da3_types_exp",
-    "da6": "experiments/da6_exp"
+    "da6": "experiments/da6_exp",
+    "dra3": "experiments/dra3_exp"
 }
