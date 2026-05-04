b7a4ec65a1ce7f4858eea8602043c08b13eb3644
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2026-05-04 18:38:18 +0900

    Fixed typo

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ warnings.simplefilter("ignore")
 
 logger = initialize_logging(__name__)
 
-@hydra.main(config_path="configs", config_name=config_names["LangStrat_da6_iqn"])
+@hydra.main(config_path="configs", config_name=config_names["LangStrat_lgsfda6_iqn_step0"])
 def main(config: DictConfig):
     os.environ["CUDA_VISIBLE_DEVICES"] = str(config.gpu)
     set_seed(seed=config.seed)
