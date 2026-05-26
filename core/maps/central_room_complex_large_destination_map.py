2636e1b713037a6cc5502867cafa9f5a93e64984
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2026-05-27 01:09:15 +0900

    updated to 5 areas

--- core/maps/central_room_complex_large_destination_map.py
+++ core/maps/central_room_complex_large_destination_map.py
@@ -10,11 +10,12 @@ from .central_room_complex_large_map import CentralRoomComplexLargeMap
 
 class CentralRoomComplexLargeDestinationMap(CentralRoomComplexLargeMap):
     def reset_destination_area(self):
-        destination_area = [np.zeros(shape=(self.SIZE_X, self.SIZE_Y), dtype=np.int8) for _ in range(4)]
+        destination_area = [np.zeros(shape=(self.SIZE_X, self.SIZE_Y), dtype=np.int8) for _ in range(5)]
         destination_area[0][: self.SIZE_X // 2, :] = 1  # left half
         destination_area[1][self.SIZE_X // 2:, :] = 1  # right half
         destination_area[2][:, : self.SIZE_Y // 2] = 1  # top half
         destination_area[3][:, self.SIZE_Y // 2:] = 1  # bottom half
+        destination_area[4] = np.ones(shape=(self.SIZE_X, self.SIZE_Y), dtype=np.int8)
 
         for agent_id in range(self.num_agents):
             area_id = random.choice(range(len(destination_area)))
--- core/maps/central_room_complex_large_single_lang_destination_map.py
+++ core/maps/central_room_complex_large_single_lang_destination_map.py
@@ -22,13 +22,17 @@ class CentralRoomComplexLargeSingleLangDestinationMap(CentralRoomComplexLargeMap
         super().__init__(config=config,size_x=size_x,size_y=size_y)
 
     def reset_destination_area(self):
-        destination_area = [np.zeros(shape=(self.SIZE_X, self.SIZE_Y), dtype=np.int8) for _ in range(4)]
+        destination_area = [np.zeros(shape=(self.SIZE_X, self.SIZE_Y), dtype=np.int8) for _ in range(5)]
         destination_area[0][: self.SIZE_X // 2, :] = 1  # left half
         destination_area[1][self.SIZE_X // 2:, :] = 1  # right half
         destination_area[2][:, : self.SIZE_Y // 2] = 1  # top half
         destination_area[3][:, self.SIZE_Y // 2:] = 1  # bottom half
+        destination_area[4] = np.ones(shape=(self.SIZE_X, self.SIZE_Y), dtype=np.int8)
 
         for agent_id in range(self.num_agents):
             area_id = random.choice(range(len(destination_area)))
             self.destination_area_matrix[agent_id] = destination_area[area_id]
-            self.destination_area_vector[agent_id] = random.choice(self.tensors[area_id])
+            if area_id == 4:
+                self.destination_area_vector[agent_id] = torch.normal(0,0.05,size=(1,384))[0]
+            else:
+                self.destination_area_vector[agent_id] = random.choice(self.tensors[area_id])
